251bed5d8683abf87f96182cc9182c8afb36b625
Murisocratis <Lorenzo.holandam@gmail.com>
2023-02-15 16:10:57 +0000

    MOTHMAN FIX

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
         </p>
      <h1>Mothman isn't real???</h1>
-     <image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC37S0CpC4i0XWwSX0-MZhg6TwwxhYBNsCuCCAs7xNjz9-lQfErOv6suwkME8FA0CEAiA&usqp=CAU"/>
+     <img src="https://64.media.tumblr.com/d215f2643738ccbd34d65f62dece436c/tumblr_pkjawd0neo1vw8ied_1280.jpg"/>
      <h1>Explain this then.</h1>
       </header>
     </div>
