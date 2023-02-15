34852d22d2551d9ed63113b59cbf4b7be65ad42f
Murisocratis <Lorenzo.holandam@gmail.com>
2023-02-15 15:30:00 +0000

    fix #issue-1586049457

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
         </p>
-     <h1>Mothman is real</h1>
+     <h1>Mothman isn't real??</h1>
+     <image src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC37S0CpC4i0XWwSX0-MZhg6TwwxhYBNsCuCCAs7xNjz9-lQfErOv6suwkME8FA0CEAiA&usqp=CAU"/>
+     <h1>Explain this then.</h1>
       </header>
     </div>
   );
